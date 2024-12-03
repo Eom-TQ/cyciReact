39cbfe6fc785cc2b0714c5104a7c293490f424a6
eom <djaxorb3091@naver.com>
2024-12-03 13:33:17 +0900

    Router 기능

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Component를 관리하는 공간.
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/"} element={<Home />}></Route>
+          <Route path={"/about"} element={<About />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
+}
+
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Link to="/about">About으로 이동</Link>
+    </div>
+  )
+}
+
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <Link to="/">Home로 이동</Link>
+    </div>
+  )
 }
 
 export default App;
