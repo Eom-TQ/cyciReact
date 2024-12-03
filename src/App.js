6bbbca728c185525e381d9b0af0386fdd35e20ff
eom <djaxorb3091@naver.com>
2024-12-03 13:42:28 +0900

    컴포넌트 까먹음

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
+import Calc1 from './comp/calc/study01'
+
 function App() {
   return (
     <div className="App">
-      Component를 관리하는 공간.
       <BrowserRouter>
+        <About></About>
         <Routes>
           <Route path={"/"} element={<Home />}></Route>
           <Route path={"/about"} element={<About />}></Route>
+          <Route path={"/calc1"} element={<Calc1></Calc1>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
@@ -20,16 +23,19 @@ function Home() {
   return (
     <div>
       <h1>Home</h1>
-      <Link to="/about">About으로 이동</Link>
+      <Link to="/about">About으로 이동</Link><br />
+      <Link to="/calc1">Calc1로 이동</Link>
     </div>
   )
 }
 
 function About() {
   return (
-    <div>
-      <h1>About</h1>
-      <Link to="/">Home로 이동</Link>
+    <div style={{
+      border: "2px solid blue"
+    }}>
+
+      <Link to="/">Home로 이동</Link><br />
     </div>
   )
 }
